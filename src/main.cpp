353de602a61c131d8a66faff917126ed5aefd27d
knifemaster <emilasanbekov@gmail.com>
2024-12-12 15:10:38 +0600

    added example and include dirs glew glut to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,175 @@
-#define GLFW_INCLUDE_NONE
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <iostream>
-#include <vector>
-#include <cmath>
-//#define GLAD_GL_IMPLEMENTATION
-//#include <glad/gl.h>
-//#define GLFW_INCLUDE_NONE
-//#include <GLFW/glfw3.h>
+#include <cstdio>
+using namespace std;
 
-const unsigned int WIDTH = 800;
-const unsigned int HEIGHT = 600;
-
-const int SEGMENTS = 50;
-
-const float radius = 0.5f;
-const float HEIGHT_CYLINDER = 1.0f;
-
-const double PI  = 3.141592653589793238463;
+GLint vertexID;
+GLint fragID;
+GLuint shader;
 
 
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
+// You shouldn't have to call this function (it's called from the functions
+// that make shaders); it is a helper function to determine if a shader compiled correctly.
+int compiledStatus(GLint shaderID){
+	GLint compiled = 0;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compiled);
+	if (compiled) {
+		return true;
+	}
+	else {
+		GLint logLength;
+		glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &logLength);
+		char* msgBuffer = new char[logLength];
+		glGetShaderInfoLog(shaderID, logLength, NULL, msgBuffer);
+		printf ("%s\n", msgBuffer);
+		delete (msgBuffer);
+		return false;
 	}
 }
 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
+// This function takes in the source code for a vertex shader,
+// compiles it, and returns the ID (number) of the shader. It returns
+// -1 if the shader didn't compile
+GLint makeVertexShader (const char* shaderSource) {
+	GLint vShaderID = glCreateShader(GL_VERTEX_SHADER);
+	cout << "createshader" << endl;
+	glShaderSource (vShaderID, 1, (const GLchar**)&shaderSource, NULL);
+	glCompileShader(vShaderID);
+	int compiledCorrectly = compiledStatus(vShaderID);
+	if (compiledCorrectly) {
+		return vShaderID;
+	}
+	return -1;
 }
 
-
-void generateCylinder(std::vector<float>& vertices, std::vector<unsigned int>& indices) {
-
-	for (int i = 0; i <= SEGMENTS; ++i) {
-		float angle = 2.0f * PI * i / SEGMENTS;
-		float x = RADIUS * std::cos(angle);
-		float z = RADIUS * std::sin(angle);
-
-		vertices.push_back(x);
-		vertices.push_back(HEIGHT_CYLINDER / 2.0f);
-		vertices.push_back(z);
-		vertices.push_back(x);
-		vertices.push_back(-HEIGHT_CYLINDER / 2.0f);
-		vertices.push_back(z);
+// This function takes in the source code for a fragment shader,
+// compiles it, and returns the ID (number) of the shader. It returns
+// -1 if the shader didn't compile
+GLuint makeFragmentShader (const char* shaderSource) {
+	GLint fShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource (fShaderID, 1, (const GLchar**)&shaderSource, NULL);
+	glCompileShader(fShaderID);
+	int compiledCorrectly = compiledStatus(fShaderID);
+	if (compiledCorrectly) {
+		return fShaderID;
 	}
+	return -1;
+}
 
-	for (int i = 0; i < SEGMENTS; ++i) {
-		indices.push_back(i * 2);
-		indices.push_back(i * 2 + 1);
-		indices.push_back((i * 2 + 2) % (SEGMENTS * 2));
-		indices.push_back(i * 2 + 1);
-		indices.push_back((i * 2 + 3) % (SEGMENTS * 2));
-		indices.push_back((i * 2 + 2) % (SEGMENTS * 2));	
+// A helper function that takes in a program ID and returns whether
+// or not it linked correctly.  If not, it prints out an error
+// message letting you know what the problem was. This will
+// be called from makeShaderProgram()
+bool linkedStatus(GLint programID){
+	GLint linked = 0;
+	glGetProgramiv(programID, GL_LINK_STATUS, &linked);
+	if (linked) {
+		return true;
+	}
+	else {
+		GLint logLength;
+		glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &logLength);
+		char* msgBuffer = new char[logLength];
+		glGetProgramInfoLog(programID, logLength, NULL, msgBuffer);
+		printf ("%s\n", msgBuffer);
+		delete (msgBuffer);
+		return false;
+	}
+}
 
+// A function that takes in the IDs for vertex and fragment shader,
+// and returns the ID of a shader program that has been linked.
+GLint makeShaderProgram (GLint vertexShaderID, GLint fragmentShaderID) {
+	printf ("Linking %d with %d\n", vertexShaderID, fragmentShaderID);
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram (programID);
+
+	bool linked = linkedStatus (programID);
+
+	if (linked) {
+		return programID;
+	}
+	return -1;
+}
+
+// Any time the window is resized, this function gets called.  It's setup to the 
+// "glutReshapeFunc" in main.
+void changeViewport(int w, int h){
+	glViewport(0, 0, w, h);
+}
+
+// Here is the function that gets called each time the window needs to be redrawn.
+// It is the "paint" method for our program, and is set up from the glutDisplayFunc in main
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1,0.5,0.5);
+		glVertex3f(-1,1,1);
+		glColor3f(0.5,1,0.5);
+
+		glVertex3f(1,1, 0);
+		glVertex3f(1,-1,1);
+
+		glColor3f(0.5,0.5,0.5);
+		glVertex3f(-1,1,1);
+		glVertex3f(-1,-1, 1);
+		glVertex3f(1,-1,1);
+	glEnd();
+	glutSwapBuffers();
+}
+
+int main (int argc, char** argv) {
+	// Initialize GLUT
+	glutInit(&argc, argv);
+
+	cout << "gi " << endl;
+	// Set up some memory buffers for our display
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+	// Set the window size
+	glutInitWindowSize(800, 600);
+	// Create the window with the title "Triangle"
+	glutCreateWindow("Triangle");
+	// Bind the two functions (above) to respond when necessary
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+	cout << "all gi " << endl;
+
+	// Very important!  This initializes the entry points in the OpenGL driver so we can 
+	// call all the functions in the API.
 	
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
 	}
+	
+	const char* vertexProg = "#version 120\n\
+					   void main () {\n\
+							gl_Position = gl_Vertex;\
+							gl_FrontColor=gl_Color;\
+					   }\n";
 
-}
+	const char* fragmentProg="#version 120\n\
+					   void main () {\n\
+					   gl_FragColor = gl_Color;\n\
+					   }\n";
 
 
-int main() {
+	vertexID = makeVertexShader (vertexProg);
+	fragID = makeFragmentShader (fragmentProg);
+	shader = makeShaderProgram(vertexID, fragID);
 
-	GLFWwindow* window;
+	glUseProgram (shader);
 
-	if (!glfwInit()) {
-		return -1;
-	}
+	cout << "Vertex ID is " << vertexID << endl;
+	cout << "Fragment ID is " << fragID << endl;
+	cout << "Shader program ID is " << shader << endl;
 
-	window = glfwCreateWindow(640, 480, "Octavian", NULL, NULL);
-	glfwMakeContextCurrent(window);
-
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "couldn-t load opengl" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glClearColor(0.10f, 0.25f, 0.50f, 1.0f);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-	}
-
-	//std::cout << "Hello world" << std::endl;
-
-	glfwTerminate();
-
+	// Start up a loop that runs in the background (you never see it).
+	glutMainLoop();
 	return 0;
 }
---
+++ src/main_old.cpp
@@ -0,0 +1,97 @@
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+//#define GLAD_GL_IMPLEMENTATION
+//#include <glad/gl.h>
+//#define GLFW_INCLUDE_NONE
+//#include <GLFW/glfw3.h>
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+const int SEGMENTS = 50;
+
+const float RADIUS = 0.5f;
+const float HEIGHT_CYLINDER = 1.0f;
+
+const double PI  = 3.141592653589793238463;
+
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+void generateCylinder(std::vector<float>& vertices, std::vector<unsigned int>& indices) {
+
+	for (int i = 0; i <= SEGMENTS; ++i) {
+		float angle = 2.0f * PI * i / SEGMENTS;
+		float x = RADIUS * std::cos(angle);
+		float z = RADIUS * std::sin(angle);
+
+		vertices.push_back(x);
+		vertices.push_back(HEIGHT_CYLINDER / 2.0f);
+		vertices.push_back(z);
+		vertices.push_back(x);
+		vertices.push_back(-HEIGHT_CYLINDER / 2.0f);
+		vertices.push_back(z);
+	}
+
+	for (int i = 0; i < SEGMENTS; ++i) {
+		indices.push_back(i * 2);
+		indices.push_back(i * 2 + 1);
+		indices.push_back((i * 2 + 2) % (SEGMENTS * 2));
+		indices.push_back(i * 2 + 1);
+		indices.push_back((i * 2 + 3) % (SEGMENTS * 2));
+		indices.push_back((i * 2 + 2) % (SEGMENTS * 2));	
+
+	
+	}
+
+}
+
+
+int main() {
+
+	GLFWwindow* window;
+
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "Octavian", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "couldn-t load opengl" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glClearColor(0.10f, 0.25f, 0.50f, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	//std::cout << "Hello world" << std::endl;
+
+	glfwTerminate();
+
+	return 0;
+}
